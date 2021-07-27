4fbbe70ddba0ce79801c872eb3d2f47a37f25964
Celestialknig24 <66995275+Celestialknig24@users.noreply.github.com>
2021-07-27 12:53:21 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('D:\\Mini Projects\\no of face detection\\haarcascade_frontalface_default.xml')
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    i=0
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        i=i+1
+        cv2.putText(img,'Face_'+str(i),(x-10,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        #print(face,i)
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
